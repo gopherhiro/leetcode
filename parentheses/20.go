ad8d262f16a158899beef1665f921efe3712e4c7
guohailong11726 <guohailong11726@ipalfish.com>
2022-06-15 17:06:05 +0800

    feat: coding again

--- parentheses/20.go
+++ parentheses/20.go
@@ -73,3 +73,47 @@ func leftOf(ch byte) byte {
 	}
 	return '['
 }
+
+// 20. Valid Parentheses
+// 20. 有效的括号
+// time O(N), space O(N)
+func isValid06(s string) bool {
+	if len(s) == 0 {
+		return false
+	}
+	stack := make([]byte, 0)
+	for i := 0; i < len(s); i++ {
+		if isLeft(s[i]) {
+			stack = append(stack, s[i])
+			continue
+		}
+		if len(stack) == 0 {
+			return false
+		}
+		top := stack[len(stack)-1]
+		if leftOf(s[i]) == top {
+			stack = stack[:len(stack)-1]
+		} else {
+			return false
+		}
+	}
+	// 如果栈空，则是有效括号
+	if len(stack) == 0 {
+		return true
+	}
+	return false
+}
+
+func leftOf06(ch byte) byte {
+	if ch == '}' {
+		return '{'
+	}
+	if ch == ']' {
+		return '['
+	}
+	return '('
+}
+
+func isLeft(ch byte) bool {
+	return ch == '{' || ch == '[' || ch == '('
+}
