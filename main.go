4f74144e8971cc24dccbb4f083f9a572e509ca9d
guohailong11726 <guohailong11726@ipalfish.com>
2022-10-02 09:16:34 +0800

    feat: daily question

--- main.go
+++ main.go
@@ -3,14 +3,39 @@ package main
 import "fmt"
 
 func main() {
-	r := smallestEvenMultiple(6)
+	r := countBits2(3)
 	fmt.Println(r)
 }
 
-func smallestEvenMultiple(n int) int {
-	isOdd := n%2 != 0
-	if isOdd {
-		return 2 * n
+func countBits2(n int) []int {
+	ans := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		// 二进制表示中
+		// 奇数中 1 的个数，比前面那个偶数多一个 1
+		// 偶数中 1 的个数，和除以 2 之后的那个数一样多
+		if i%2 == 1 {
+			ans[i] = ans[i-1] + 1
+		} else {
+			ans[i] = ans[i/2]
+		}
 	}
-	return n
+	return ans
+}
+
+func countBits(n int) []int {
+	ans := make([]int, 0, n+1)
+	for i := 0; i <= n; i++ {
+		res := countBit(i)
+		ans = append(ans, res)
+	}
+	return ans
+}
+
+func countBit(num int) int {
+	count := 0
+	for num > 0 {
+		num = num & (num - 1)
+		count++
+	}
+	return count
 }
---
+++ math/338.go
@@ -0,0 +1,43 @@
+package math
+
+// 338. Counting Bits
+// 338. 比特位计数
+// 思路：二进制位运算
+// time O(nlogn) space O(n)
+func countBits(n int) []int {
+	ans := make([]int, 0, n+1)
+	for i := 0; i <= n; i++ {
+		res := countBit(i)
+		ans = append(ans, res)
+	}
+	return ans
+}
+
+// time O(logn) space O(1)
+func countBit(num int) int {
+	count := 0
+	for num > 0 {
+		num = num & (num - 1)
+		count++
+	}
+	return count
+}
+
+// 338. Counting Bits
+// 338. 比特位计数
+// 思路：二进制位规律统计
+// time O(n) space O(n)
+func countBits2(n int) []int {
+	ans := make([]int, n+1)
+	for i := 1; i <= n; i++ {
+		// 二进制表示中
+		// 奇数中 1 的个数，比前面「那个偶数」多一个 1
+		// 偶数中 1 的个数，和「除以 2 之后的那个数」一样多
+		if i%2 == 1 {
+			ans[i] = ans[i-1] + 1
+		} else {
+			ans[i] = ans[i/2]
+		}
+	}
+	return ans
+}
