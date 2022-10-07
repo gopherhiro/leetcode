1cbd2946546e72c5f88e34ffea7885ba6aaa9275
guohailong11726 <guohailong11726@ipalfish.com>
2022-10-07 12:08:48 +0800

    feat: daily question

--- string/387.go
+++ string/387.go
@@ -1,13 +1,22 @@
-package main
+package string
 
-import (
-	"fmt"
-)
-
-func main() {
-	s := "aabb"
-	r := firstUniqChar2(s)
-	fmt.Println(r)
+// 387. First Unique Character in a String
+// 387. 字符串中的第一个唯一字符
+// 思路：哈希表统计频次
+// time O(n) time O(1)
+func firstUniqChar(s string) int {
+	ht := make(map[rune]int, 0)
+	// 第一次遍历：统计频次
+	for _, ch := range s {
+		ht[ch]++
+	}
+	// 第二次遍历：找寻第一个频次为 1 的字符下标即可
+	for i, ch := range s {
+		if ht[ch] == 1 {
+			return i
+		}
+	}
+	return -1
 }
 
 // 387. First Unique Character in a String
@@ -41,21 +50,4 @@ func min(x, y int) int {
 		return x
 	}
 	return y
-}
-
-// 387. First Unique Character in a String
-// 387. 字符串中的第一个唯一字符
-// 思路：哈希表
-// time O(n) time O(1)
-func firstUniqChar(s string) int {
-	ht := make(map[rune]int, 0)
-	for _, ch := range s {
-		ht[ch]++
-	}
-	for i, ch := range s {
-		if ht[ch] == 1 {
-			return i
-		}
-	}
-	return -1
 }
