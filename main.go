fc551410e925f330143ed030e83147525c10056f
guohailong11726 <guohailong11726@ipalfish.com>
2022-10-07 16:49:32 +0800

    feat: lccup22

--- main.go
+++ main.go
@@ -5,57 +5,8 @@ import (
 )
 
 func main() {
-	s := "aabb"
-	r := firstUniqChar2(s)
+	flowers := []int{1, 2, 3, 2}
+	cnt := 1
+	r := beautifulBouquet(flowers, cnt)
 	fmt.Println(r)
-}
-
-// 387. First Unique Character in a String
-// 387. 字符串中的第一个唯一字符
-// 思路：哈希表存储索引
-// time O(n) time O(1)
-func firstUniqChar2(s string) int {
-	ht := make(map[rune]int, 0)
-	for i, ch := range s {
-		if _, ok := ht[ch]; ok {
-			ht[ch] = -1
-		} else {
-			ht[ch] = i
-		}
-	}
-	first := len(s)
-	for _, index := range ht {
-		if index == -1 {
-			continue
-		}
-		first = min(first, index)
-	}
-	if first == len(s) {
-		first = -1
-	}
-	return first
-}
-
-func min(x, y int) int {
-	if x < y {
-		return x
-	}
-	return y
-}
-
-// 387. First Unique Character in a String
-// 387. 字符串中的第一个唯一字符
-// 思路：哈希表
-// time O(n) time O(1)
-func firstUniqChar(s string) int {
-	ht := make(map[rune]int, 0)
-	for _, ch := range s {
-		ht[ch]++
-	}
-	for i, ch := range s {
-		if ht[ch] == 1 {
-			return i
-		}
-	}
-	return -1
 }
--- weeklycontest/lccup22.go
+++ weeklycontest/lccup22.go
@@ -58,3 +58,138 @@ func compare(before, after int) int {
 	}
 	return -1
 }
+
+// 最小展台数量
+// 思路：哈希表
+// time O(mn) space O(1)
+// https://leetcode.cn/contest/season/2022-fall/problems/600YaG/
+func minNumBooths(demand []string) int {
+	ht := make(map[rune]int, 0)
+	for _, str := range demand {
+		m := make(map[rune]int, 0)
+		for _, ch := range str {
+			m[ch]++
+		}
+		for char, count := range m {
+			v, ok := ht[char]
+			// 不存在
+			if !ok {
+				ht[char] = count
+				continue
+			}
+			// 存在
+			if ok && count > v {
+				ht[char] = count
+			}
+		}
+	}
+	sum := 0
+	for _, count := range ht {
+		sum += count
+	}
+	return sum
+}
+
+// TreeNode Definition for a binary tree node.
+type TreeNode struct {
+	Left  *TreeNode
+	Val   int
+	Right *TreeNode
+	Next  *TreeNode
+}
+
+/**
+ * Definition for a binary tree node.
+ * type TreeNode struct {
+ *     Val int
+ *     Left *TreeNode
+ *     Right *TreeNode
+ * }
+ */
+// 装饰树
+// 思路：BFS
+// time O(n) space O(n)
+// https://leetcode.cn/contest/season/2022-fall/problems/KnLfVT/
+func expandBinaryTree(root *TreeNode) *TreeNode {
+	if root == nil {
+		return nil
+	}
+	queue := []*TreeNode{root}
+	for len(queue) > 0 {
+		levelSize := len(queue)
+		for i := 0; i < levelSize; i++ {
+			cur := queue[0]
+			queue = queue[1:]
+			if cur.Left != nil {
+				queue = append(queue, cur.Left)
+				new := &TreeNode{Val: -1}
+				tmp := cur.Left
+				cur.Left = new
+				new.Left = tmp
+			}
+			if cur.Right != nil {
+				queue = append(queue, cur.Right)
+				new := &TreeNode{Val: -1}
+				tmp := cur.Right
+				cur.Right = new
+				new.Right = tmp
+			}
+		}
+	}
+	return root
+}
+
+// 美观的花束
+// 思路：回溯
+func beautifulBouquet(flowers []int, target int) [][]int {
+	res := make([][]int, 0)
+
+	// 记录已选择路径
+	track := make([]int, 0)
+
+	var backtrack func(nums []int, start int)
+	backtrack = func(nums []int, start int) {
+		// 前序位置：收集结果。
+		tmp := make([]int, len(track))
+		copy(tmp, track)
+
+		// 判断条件
+		if !isBeautiful(tmp, target) {
+			return
+		}
+
+		res = append(res, tmp)
+
+		// 使用 start 参数控制递归，进行剪枝。
+		for i := start; i < len(nums); i++ {
+
+			// 剪枝。值相同的相邻树枝，只遍历第一条。
+			if i > start && nums[i] == nums[i-1] {
+				continue
+			}
+
+			// 做选择
+			track = append(track, nums[i])
+
+			// 进入下一层抉择树
+			backtrack(nums, i+1)
+
+			// 撤销选择
+			track = track[:len(track)-1]
+		}
+
+	}
+	backtrack(flowers, 0)
+	return res
+}
+
+func isBeautiful(flowers []int, count int) bool {
+	ht := make(map[int]int, 0)
+	for _, v := range flowers {
+		ht[v]++
+		if ht[v] > count {
+			return false
+		}
+	}
+	return true
+}
