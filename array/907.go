051a6d95b357d2816fae201adbb8326f62221e75
guohailong11726 <guohailong11726@ipalfish.com>
2022-10-28 21:06:11 +0800

    daily question

--- array/907.go
+++ array/907.go
@@ -2,10 +2,46 @@ package array
 
 // 907. Sum of Subarray Minimums
 // 907. 子数组的最小值之和
+// 思路：单调栈
+// time O(n) time O(n)
+func sumSubarrayMins(arr []int) int {
+	const MOD int = 1e9 + 7
+	stack := make([]int, 0)
+	sum := 0
+	for i := 0; i <= len(arr); i++ {
+		// when i reaches the array length, it is an indication that
+		// all the elements have been processed, and the remaining
+		// elements in the stack should now be popped out.
+		for len(stack) != 0 && (i == len(arr) || arr[stack[len(stack)-1]] >= arr[i]) {
+			// Notice the sign ">=", This ensures that no contribution
+			// is counted twice. rightBoundary takes equal or smaller
+			// elements into account while leftBoundary takes only the
+			// strictly smaller elements into account
+			mid := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			// left boundary and right boundary
+			left := -1
+			if len(stack) != 0 {
+				left = stack[len(stack)-1]
+			}
+			right := i
+
+			// count of subarrays where mid is the minimum element
+			count := (mid - left) * (right - mid)
+			sum += count * arr[mid]
+		}
+		stack = append(stack, i)
+	}
+	return sum % MOD
+}
+
+// 907. Sum of Subarray Minimums
+// 907. 子数组的最小值之和
 // 思路：暴力破解
 // time O(n^2) space O(1)
 // 备注：超时
-func sumSubarrayMins(arr []int) int {
+func sumSubarrayMins2(arr []int) int {
 	n := len(arr)
 	sumMin := 0
 	for start := 0; start < n; start++ {
--- main.go
+++ main.go
@@ -5,7 +5,43 @@ import (
 )
 
 func main() {
-	nums := []int{3, 1, 2, 4}
+	nums := []int{3, 4, 4, 5, 4, 1}
 	r := sumSubarrayMins(nums)
 	fmt.Println(r)
+}
+
+// 907. Sum of Subarray Minimums
+// 907. 子数组的最小值之和
+// 思路：单调栈
+// time O(n) time O(n)
+func sumSubarrayMins(arr []int) int {
+	const MOD int = 1e9 + 7
+	stack := make([]int, 0)
+	sum := 0
+	for i := 0; i <= len(arr); i++ {
+		// when i reaches the array length, it is an indication that
+		// all the elements have been processed, and the remaining
+		// elements in the stack should now be popped out.
+		for len(stack) != 0 && (i == len(arr) || arr[stack[len(stack)-1]] >= arr[i]) {
+			// Notice the sign ">=", This ensures that no contribution
+			// is counted twice. rightBoundary takes equal or smaller
+			// elements into account while leftBoundary takes only the
+			// strictly smaller elements into account
+			mid := stack[len(stack)-1]
+			stack = stack[:len(stack)-1]
+
+			// left boundary and right boundary
+			left := -1
+			if len(stack) != 0 {
+				left = stack[len(stack)-1]
+			}
+			right := i
+
+			// count of subarrays where mid is the minimum element
+			count := (mid - left) * (right - mid)
+			sum += count * arr[mid]
+		}
+		stack = append(stack, i)
+	}
+	return sum % MOD
 }
